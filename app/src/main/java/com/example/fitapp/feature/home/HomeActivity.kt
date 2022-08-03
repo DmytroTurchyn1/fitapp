22b8266b0c94e27f7f215b1aa2764d43b029cbb4
Dima <66136912+DimaLabsdeveloper@users.noreply.github.com>
2022-08-03 22:39:07 +0200

    Added: * bottom menu * fragments Changed:

--- app/src/main/java/com/example/fitapp/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/fitapp/feature/home/HomeActivity.kt
@@ -2,16 +2,42 @@ package com.example.fitapp.feature.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.fitapp.R
 import com.example.fitapp.feature.login.MainPresenter
+import com.example.fitapp.fragments.AccountFragment
+import com.example.fitapp.fragments.CalendarFragment
+import com.example.fitapp.fragments.WorkoutFragment
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity(), HomeView {
     private lateinit var presenter: HomePresenter
+    private var WorkoutFragment = WorkoutFragment()
+    private var CalendarFragment = CalendarFragment()
+    private var AccountFragment = AccountFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         presenter = HomePresenter(this)
+        replace_fragment(WorkoutFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.Workout -> replace_fragment(WorkoutFragment)
+                R.id.calendar -> replace_fragment(WorkoutFragment)
+                R.id.account -> replace_fragment(WorkoutFragment)
+            }
+            true
+        }
     }
 
+    private fun replace_fragment(fragment: Fragment){
+        if (fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+    }
 }
